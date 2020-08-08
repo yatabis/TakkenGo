66d7f9eb075decd08bd7c0cea9d08dcece03a2c6
yatabis <39yatabis.rim8820xxx@gmail.com>
2020-08-08 21:00:27 +0900

    add: LINE の最低限のリプライを実装

---
+++ line/bot.go
@@ -0,0 +1,44 @@
+package line
+
+import (
+	"log"
+	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+type LINE struct {
+	Client *linebot.Client
+}
+
+func New() *LINE {
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_ACCESS_TOKEN"),
+	)
+	if err != nil {
+		log.Printf("failed to create new line bot.: %e\n", err)
+		return nil
+	}
+	return &LINE{Client: bot}
+}
+
+func (b *LINE) ReplyOtherType(token string) {
+	text := "テキスト以外のメッセージタイプには対応していません。"
+	res, err := b.Client.ReplyMessage(token, linebot.NewTextMessage(text)).Do()
+	if err == nil {
+		log.Printf("replied to other message type: %+v\n", res)
+	} else {
+		log.Printf("failed to replying to other message type: %e\n", err)
+	}
+}
+
+func (b *LINE) ReplyOtherText(token string) {
+	text := "テキストを受け取りました。\n解答を登録する場合は、解答する問題の「解答」ボタンを押してから点数を入力してください。"
+	res, err := b.Client.ReplyMessage(token, linebot.NewTextMessage(text)).Do()
+	if err == nil {
+		log.Printf("replied to other text message: %+v\n", res)
+	} else {
+		log.Printf("failed to replying to other text message: %e\n", err)
+	}
+}
--- line/callback.go
+++ line/callback.go
@@ -1,11 +1,40 @@
 package line
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func Callback(c echo.Context) error {
+	bot := New()
+	if bot == nil {
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+	events, err := bot.Client.ParseRequest(c.Request())
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			log.Printf("receive the bad request: %e\n", err)
+			return c.JSON(http.StatusBadRequest, err)
+		} else {
+			log.Printf("unexpected error occurs: %e\n", err)
+			return c.JSON(http.StatusInternalServerError, err)
+		}
+	}
+
+	for _, event := range events {
+		token := event.ReplyToken
+		if event.Type != linebot.EventTypeMessage {
+			break
+		}
+		switch event.Message.(type) {
+		case *linebot.TextMessage:
+			bot.ReplyOtherText(token)
+		default:
+			bot.ReplyOtherType(token)
+		}
+	}
 	return c.String(http.StatusOK, "OK")
 }
