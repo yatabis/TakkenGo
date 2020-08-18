b18f87843714ad95be21b785ab720d140ed13e01
yatabis <39yatabis.rim8820xxx@gmail.com>
2020-08-18 17:40:59 +0900

    add: スコアをデータベースに保存する処理を実装

--- database/db.go
+++ database/db.go
@@ -2,8 +2,10 @@ package database
 
 import (
 	"database/sql"
+	"errors"
 	"log"
 	"os"
+	"strconv"
 
 	_ "github.com/lib/pq"
 )
@@ -42,4 +44,65 @@ func GetQuestionsById(id int) (chapter, section string) {
 		log.Println(err)
 	}
 	return
+}
+
+func GetScore(time int) (score int) {
+	db := openDB()
+	defer closeDB(db)
+	row := db.QueryRow("select score from scores where time = $1", time)
+	if err := row.Scan(&score); err != nil {
+		log.Println(err)
+	}
+	return
+}
+
+func UpdateScore(time, score int) error {
+	db := openDB()
+	defer closeDB(db)
+	_, err := db.Exec("update scores set score = $1 where time = $2", score, time)
+	if err != nil {
+		log.Println(err)
+		return errors.New("スコアの保存に失敗しました。")
+	}
+	return nil
+}
+
+func UpdateRate(id, score int) error {
+	db := openDB()
+	defer closeDB(db)
+	var rate, count float64
+	row := db.QueryRow("select rate, count from questions where id = $1", id)
+	err := row.Scan(&rate, &count)
+	if err != nil {
+		log.Println(err)
+		return errors.New("スコアの保存に失敗しました。")
+	}
+	rate = (rate * count + float64(score)) / (count + 1)
+	_, err = db.Exec("update questions set rate = $1, count = $2 where id = $3", rate, count + 1, id)
+	if err != nil {
+		log.Println(err)
+		return errors.New("スコアの保存に失敗しました。")
+	}
+	return nil
+}
+
+func SaveScore(id, time, score int) error {
+	if GetScore(time) != 0 {
+		return errors.New(strconv.Itoa(time) + "時のスコアはすでに登録されています。")
+	}
+	if err := UpdateScore(time, score); err != nil {
+		return err
+	}
+	if err := UpdateRate(id, score); err != nil {
+		return err
+	}
+	return nil
+}
+
+func ResetScores() {
+	db := openDB()
+	defer closeDB(db)
+	if _, err := db.Exec("update scores set score = 0"); err != nil {
+		log.Println(err)
+	}
 }
